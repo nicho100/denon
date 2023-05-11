c277f9685a6f5041d883f8071dc79f7b592acd8c
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-11 17:23:52 -0300

    Update index.ts

--- routes/index.ts
+++ routes/index.ts
@@ -5,3 +5,5 @@ export const productRouter = new Router()
   .get('/api/products/:productId', findProduct)
   .get('/api/products', listProducts)
   .post('/api/products', createProduct);
+  .put('/api/products/:productId', updateProduct)
+  .delete('/api/products/:productId', deleteProduct)
